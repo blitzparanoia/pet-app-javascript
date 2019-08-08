913aef6fa98b0540144fe7d0f540ddd96b6d290a
blitzparanoia <bluemilk44@gmail.com>
2019-08-08 17:57:52 -0400

    sitters controller params permit identifed

--- app/controllers/sitters_controller.rb
+++ app/controllers/sitters_controller.rb
@@ -1,6 +1,16 @@
 class SittersController < ApplicationController
 
 def new
+  @sitter = Sitter.new
+end
+
+def create
+end
+
+private
+
+def user_params
+  params.require(:user).permit(:name, :email, :password)
 end
 
 end
