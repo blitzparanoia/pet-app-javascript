e4bd70a2ffcad98045cc828756192ee71b98a6a4
blitzparanoia <bluemilk44@gmail.com>
2019-08-08 21:35:58 -0400

    added comments/notes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,5 @@
 class SessionsController < ApplicationController
+
+#methods needed new, create, home, destroy 
+
 end
--- app/models/sitter.rb
+++ app/models/sitter.rb
@@ -1,3 +1,5 @@
 class Sitter < ApplicationRecord
   has_secure_password
+
+  #validate email with presenece and uniquness true
 end
