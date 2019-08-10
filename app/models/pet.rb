60220a2dec6493f5376c41e8c67c282f3d3c8896
blitzparanoia <bluemilk44@gmail.com>
2019-08-10 18:46:01 -0400

    added associations to models

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,2 +1,4 @@
 class Pet < ApplicationRecord
+  has_many :jobs
+  has_many :users, through: :jobs
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :jobs
+  has_many :pets, through: :jobs
 
   #validate email with presenece and uniquness true
 end
