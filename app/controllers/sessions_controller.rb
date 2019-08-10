9e77f29f189158172ab53862c23eb02ba41dd82a
blitzparanoia <bluemilk44@gmail.com>
2019-08-10 17:01:37 -0400

    Log out working, login working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,12 +2,15 @@ class SessionsController < ApplicationController
 
 #methods needed new, create, home, destroy
 def new
+  @user = User.new
 end
 
 def create
 end
 
 def destroy
+  session.clear
+  redirect_to '/'
 end
 
 end
