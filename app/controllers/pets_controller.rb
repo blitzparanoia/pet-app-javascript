dc73ef1853334c1bcd121112259c8e68d0823be3
blitzparanoia <bluemilk44@gmail.com>
2019-08-11 14:11:36 -0400

    add show edit and update to pets controller

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -23,9 +23,31 @@ class PetsController < ApplicationController
   end
 
   def edit
+    @pet = Pet.find_by(id: params[:id])
+    if !@pet
+      redirect_to pets_path
+    end
+  end
+
+  def update
+    @pet = Pet.find_by(id: params[:id])
+    if !@pet
+      redirect_to pets_path
+    end
+    if @pet.update(pet_params)
+  redirect_to pet_path(@pet)
+else
+  render :edit
+end
   end
 
   def destroy
+    @pet = Pet.find_by(id: params[:id])
+    if !@pet
+      redirect_to pets_path
+    end
+    @pet.destroy
+    redirect_to pets_path
   end
 
   private
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,4 @@
 class Job < ApplicationRecord
   belongs_to :user
-  belongs_to :job
+  belongs_to :pet
 end
