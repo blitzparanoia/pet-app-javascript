7ecce560de9331ddc9add70299d499f0aa3826a8
blitzparanoia <bluemilk44@gmail.com>
2019-08-11 11:32:48 -0400

    added params to pet controller

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,23 @@
 class PetsController < ApplicationController
+  def new
+    @pet = Pet.new
+  end
+
+  def create
+    @pet = Pet.new()
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  private
+
+  def pet_params
+    #add job attributes
+    params.require(:pet).permit(:name, :breed)
+  end
+
 end
