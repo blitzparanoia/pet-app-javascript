809418a9e53927259e1cd8d87d2b0d170683e90d
blitzparanoia <bluemilk44@gmail.com>
2019-08-20 22:27:01 -0400

    changed redirect path to jobs

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,14 @@
 class SessionsController < ApplicationController
 
-#methods needed new, create, home, destroy
 def new
   @user = User.new
 end
 
 def create
-  @user = User.find_by(email: params[:user][:email])
+  @user = User.find_by(username: params[:user][:username])
   if @user && @user.authenticate(params[:user][:password])
     session[:user_id] = @user.id
-    redirect_to pets_path
+    redirect_to jobs_path
   else
     redirect_to '/login'
   end
