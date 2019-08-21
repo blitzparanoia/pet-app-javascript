b8c9cefd9415adf75bbee1a419edd7eb53d0c655
blitzparanoia <bluemilk44@gmail.com>
2019-08-20 22:58:51 -0400

    added a private method to DRY code

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,2 +1,9 @@
 class CompaniesController < ApplicationController
+  def index
+    @companies = Company.all.includes(:users)
+  end
+
+  def show
+    @company = Company.find_by(id: params[:id])
+  end
 end
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,40 +5,53 @@ def new
 end
 
 def create
-  @job = current_user.jobs.build(job_params)
-  if @job.save
-    redirect_to job_path(@job)
-  else
-    @job.build_company unless @job.company
-    render :new
-  end
-end
+   @job = current_user.jobs.build(job_params)
+   if @job.save
+     redirect_to job_path(@job)
+   else
+     @job.build_company unless @job.company
+     render :new
+   end
+ end
 
 def index
   @jobs = Job.all
 end
 
 def show
-  @job = Job.find_by(id: params[:id])
-end
+   set_job
+ end
 
-def edit
-  @job = Job.find_by(id: params[:id])
-end
+ def edit
+   set_job
+ end
 
-def update
-  @job = Job.find_by(id: params[:id])
-  if @job.update(job_params)
-  redirect_to job_path(@job)
-else
-  render :index
-end
+ def update
+   set_job
+   if @job.update(job_params)
+     redirect_to job_path(@job)
+   else
+     render :index
+   end
+ end
+
+def destroy
+  set_job
+  @job.destroy
+  redirect_to jobs_path
 end
 
 private
 
 def job_params
   params.require(:job).permit(:title, :position, :description)
+end
+
+def set_job
+  @job = Job.find_by(id: params[:id])
+ if !@job
+   redirect_to jobs_path
+ end
 end
 
 end
