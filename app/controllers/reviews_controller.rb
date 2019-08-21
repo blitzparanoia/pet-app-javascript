b4e1d17a951c61b64758b7e10ae22e59c3c3d48d
blitzparanoia <bluemilk44@gmail.com>
2019-08-20 23:33:24 -0400

    potential methods for review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,18 @@
 class ReviewsController < ApplicationController
+
+def index
+end
+
+def new
+end
+
+def create
+end
+
+private
+
+def review_params
+  params.require(:review).permit(:content, :rating)
+end
+
 end
