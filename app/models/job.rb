4e433d55ed0200d946918d240836f6a0cbf8af25
blitzparanoia <bluemilk44@gmail.com>
2019-08-21 20:33:47 -0400

    fixed views

--- app/models/job.rb
+++ app/models/job.rb
@@ -7,11 +7,18 @@ class Job < ApplicationRecord
   validates :description, presence: true
   validates :company_id, presence: true
   validates_associated :company
+  validates :phone_number, :presence => true ,
+                            :numericality => true,
+                            :length => { :minimum => 10, :maximum => 15 }
 
 
   def company_attributes=(attributes)
     company = Company.find_or_create_by(attributes)
        self.company = company if company.valid? || !self.company
     end
+
+#scope for job type
+scope :volunteer, -> {where(position: "Volunteer")}
+
 
 end
